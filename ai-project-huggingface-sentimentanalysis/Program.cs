2ada37d47bf8c95559efd0fa0c1c279f46880828
zehra <zehraisbr@gmail.com>
2025-07-24 20:02:22 +0300

    hugging face

---
+++ ai-project-huggingface-sentimentanalysis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+var apiKey = "js_msakSADPcldDadcAfFGbHhzLsdsDWLqOERIclfkwuWa";
+var text = "this is a test";    
+
+var modelUrl = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment";
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+var json = JsonSerializer.Serialize(new { inputs = text });
+var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+var response = await client.PostAsync(modelUrl, content);
+var result = await response.Content.ReadAsStringAsync();
